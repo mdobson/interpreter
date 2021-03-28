0491de0f625a4ad72bb1c5af7222d3edec7edd36
Matthew Dobson <mdobson4@gmail.com>
2021-03-28 11:13:00 -0400

    feat(repl): Add repl and more syntax
    This commit adds a repl and additional syntax to be parsed by the lexer.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"example.com/interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! REPL v0.0.1 of UnknownScript\n", user.Username)
+	fmt.Printf(">>Ready to receive commands<<\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
--- token/token.go
+++ token/token.go
@@ -15,11 +15,29 @@ const (
 	RBRACE    = "}"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
+	LT        = "<"
+	GT        = ">"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
+	EQ        = "=="
+	NOT_EQ    = "!="
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 type TokenType string
